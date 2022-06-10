24d8dc3b287b3c06055768d64cdbea2ba7f63a75
Ross Alexander <rossaalexander@gmail.com>
2022-06-10 10:57:32 +0200

    Improve PopularGames query and view cover size

---
+++ app/Http/Livewire/ComingSoon.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Http;
+use Livewire\Component;
+
+class ComingSoon extends Component
+{
+    public $comingSoonGames = [];
+
+    public function loadComingSoonGames()
+    {
+        $current = Carbon::today()->timestamp;
+
+        $this->comingSoonGames = Http::withHeaders(config('services.igdb'))
+            ->withBody(
+                "
+                fields name, first_release_date, platforms.abbreviation, cover.url, rating;
+            where first_release_date != null
+            & cover.url != null
+            & first_release_date >= {$current};
+            sort first_release_date asc;
+            limit 5;",
+                "text/plain"
+            )
+            ->post('https://api.igdb.com/v4/games')
+            ->json();
+    }
+
+    public function render()
+    {
+        return view('livewire.coming-soon');
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,18 +27,7 @@
                     </div>
 
                     <h2 class="text-blue-500 uppercase tracking-wide font-semibold mt-20">Coming Soon</h2>
-                    <div class="space-y-10 mt-8">
-                        <div class="flex">
-                            <a href="">
-                                <img src="" alt="game cover"
-                                     class="w-16 h-24 bg-gray-600 hover:opacity-75 transition-colors ease-in-out duration-150">
-                            </a>
-                            <div class="ml-4">
-                                <a href="" class="hover:text-gray-300">Cyberpunk 2077</a>
-                                <div class="text-gray-400 text-sm mt-1">September 16, 2020</div>
-                            </div>
-                        </div>
-                    </div>
+                    <livewire:coming-soon>
 
                 </div>
             </div>
