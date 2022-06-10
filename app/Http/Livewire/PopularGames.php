0287ddf57e9786b2b3d6cf6ca6328681ca34ed6f
Ross Alexander <rossaalexander@gmail.com>
2022-06-10 11:02:09 +0200

    Cache PopularGames results

--- app/Http/Livewire/PopularGames.php
+++ app/Http/Livewire/PopularGames.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
@@ -15,19 +16,22 @@ class PopularGames extends Component
         $before = Carbon::now()->subMonths(2)->timestamp;
         $after = Carbon::now()->addMonths(2)->timestamp;
 
-        $this->popularGames = Http::withHeaders(config('services.igdb'))
-            ->withBody(
-                "
+        $this->popularGames = Cache::remember('popular-games', 7, function () use ($before, $after) {
+            sleep(3);
+            return Http::withHeaders(config('services.igdb'))
+                ->withBody(
+                    "
                 fields name, first_release_date, platforms.abbreviation, cover.url, rating;
             where first_release_date != null
             & cover.url != null
             & (first_release_date >= {$before} & first_release_date < {$after});
             sort first_release_date desc;
             limit 12;",
-                "text/plain"
-            )
-            ->post('https://api.igdb.com/v4/games')
-            ->json();
+                    "text/plain"
+                )
+                ->post('https://api.igdb.com/v4/games')
+                ->json();
+        });
     }
 
     public function render()
